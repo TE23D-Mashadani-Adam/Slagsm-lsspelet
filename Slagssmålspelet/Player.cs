37f7aca54264496cd4819dbb8d230ab484a4296d
Adam Mashadani <adam.mashadani@elev.ga.ntig.se>
2024-09-13 10:43:22 +0200

    adbnaödkan

--- Slagssmålspelet/Player.cs
+++ Slagssmålspelet/Player.cs
@@ -6,12 +6,17 @@ public class Player
     public int randomPunshPower;
     public int amountHealthBottles;
 
-
     public void playerPunshed(){
         hp -= randomPunshPower;
     }
     public void playerHeals(){
             hp += 30;
+    }
+    public void playerDead(){
+        Console.WriteLine(playerName+" is dead!!");
+    }
+    public void playerVictory(){
+        Console.WriteLine("\n"+$"{playerName} has won the game!");
     }
 
 }
--- Slagssmålspelet/Program.cs
+++ Slagssmålspelet/Program.cs
@@ -1,11 +1,13 @@
 ﻿Console.WriteLine("Welcome to my 1 v 1 game, pls follow the instructions for how to play below:"+"\n");
 Console.WriteLine("Each player are going to have 100 hp in the beginning, and every punsh gives random power"+
-", and every player has 3 healing bottles they can use after punshing!, press enter to start the game");
+", and every player has 3 healing bottles they can use after punshing!, press enter to start the game" + "\n");
 Console.ReadLine();
 
 //Creating player objects
 Player p1 = new();
 Player p2 = new();
+//Creating object of 'Random' Class
+Random random = new();
 
 //Assigning health and amount healing bottles
 p1.hp = 100;
@@ -13,6 +15,7 @@ p2.hp = 100;
 
 p1.amountHealthBottles = 3;
 p2.amountHealthBottles = 3;
+
 
 //Writing down players names
 Console.WriteLine("Write player 1 name:");
@@ -26,40 +29,61 @@ Player player;
 
 while(p1.hp>0 && p2.hp>0){
 
+    //Generating random numbers to each player
+    p1.randomPunshPower = random.Next(10,80);
+    p2.randomPunshPower = random.Next(10,80);
+
     //Player 1
 
-    Console.WriteLine($"{p1.playerName} starts punching");
+    Console.WriteLine($"{p1.playerName}'s turn, press enter to punch" + "\n");
+    Console.ReadLine();
     p2.playerPunshed();
-    Console.WriteLine($"{p1.playerName} hp: {p1.hp}");
-    Console.WriteLine($"{p1.playerName} heal bottles: {p1.amountHealthBottles}");
-    Console.WriteLine("\n"+"Do you want to use any health bottles? Yes or no?, writing random means 'no'");
+    Console.WriteLine($"{p2.playerName} hp: {p2.hp}");
+    Console.WriteLine($"{p2.playerName} heal bottles: {p2.amountHealthBottles}");
+    if(p2.hp>0){
+    Console.WriteLine("\n"+$"{p2.playerName}, do you want to use any health bottles? Yes or no?, writing random means 'no'");
     string healthBottleAns = Console.ReadLine().ToLower();
-    if(healthBottleAns=="yes"){
-        p1.hp += 30;
-        p1.amountHealthBottles--;
-        Console.WriteLine($"{p1.playerName} hp: {p1.hp}");
+    if(healthBottleAns=="yes" && p1.hp>0){
+        p2.playerHeals();
+        p2.amountHealthBottles--;
+        Console.WriteLine("\n"+$"{p2.playerName} hp: {p2.hp} Heal bottles: {p2.amountHealthBottles}" + "\n");
     }else{
         Console.WriteLine("\n");
+    }
     }
 
     //Player 2
 
-    Console.WriteLine($"{p2.playerName} starts punching");
-    p2.playerPunshed();
-    Console.WriteLine($"{p2.playerName} hp: {p2.hp}");
-    Console.WriteLine($"{p2.playerName} heal bottles: {p2.amountHealthBottles}");
-    Console.WriteLine("\n"+"Do you want to use any health bottles? Yes or no?, writing random means 'no'");
+    Console.WriteLine($"{p2.playerName}'s turn, press enter to punch");
+    Console.ReadLine();
+    p1.playerPunshed();
+    Console.WriteLine($"{p1.playerName} hp: {p1.hp}");
+    Console.WriteLine($"{p1.playerName} heal bottles: {p1.amountHealthBottles}");
+    if(p1.hp>0){
+    Console.WriteLine("\n"+$"{p1.playerName}, do you want to use any health bottles? Yes or no?, writing random means 'no'");
     string healthBottleAns2 = Console.ReadLine().ToLower();
-    if(healthBottleAns2=="yes"){
-        p2.hp += 30;
-        p2.amountHealthBottles--;
-        Console.WriteLine($"{p2.playerName} hp: {p2.hp}");
+    
+    if(healthBottleAns2=="yes" && p1.hp>0){
+        p1.playerHeals(); 
+        p1.amountHealthBottles--;
+        Console.WriteLine($"{p1.playerName} hp: {p1.hp} Heal bottles: {p1.amountHealthBottles}" + "\n");
     }else{
         Console.WriteLine("\n");
     }
+    }
 
-    
 }
+
+if(p1.hp<=0){
+    p1.playerDead();
+    p2.playerVictory();
+}
+else if(p2.hp<=0){
+    p2.playerDead();
+    p1.playerVictory();
+}
+
+Console.WriteLine("\n"+"Press enter to end the game");
 
 Console.ReadLine();
 
